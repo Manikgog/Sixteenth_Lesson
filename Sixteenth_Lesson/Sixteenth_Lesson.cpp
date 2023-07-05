d0788798a6aa87fce0b884f11c4f3e203d7fcdf0
Yakimov Lev <tiokuhytblu@gmail.com>
2023-07-05 21:20:25 +0500

    Семнадцатый урок.

--- Sixteenth_Lesson/Sixteenth_Lesson.cpp
+++ Sixteenth_Lesson/Sixteenth_Lesson.cpp
@@ -10,10 +10,17 @@ public:
 	Base() {
 		std::cout << this << " " << "Base object created\n";
 	}
-	~Base()
+	virtual ~Base()
 	{
 		std::cout << this << " " << "Base object destroyed\n";
 	}
+	virtual int Bar(int a, bool b) = 0; // чистая виртуальная функция не имеет реализации
+										// превращает класс в абстрактный
+										// все наследники которые не имеют своей реализации
+										// тоже становятся абстрактными классами
+										// абстрактный класс - содержит одну и более чистых 
+										// виртуальных функций и не может 
+										// иметь собственных объектов
 protected:
 	int protectedMember;
 
@@ -33,6 +40,14 @@ public:
 	// publicMember остаётся публичным
 	// protectedMember становится защищённым
 	// privateMember недоступен
+	int Bar(int a, bool b) override
+	{
+		return a | b;
+	}
+	~Derived() override
+	{
+		std::cout << this << " " << "Derived object destroyed\n";
+	}
 
 private:
 
@@ -100,8 +115,10 @@ private:
 	std::string _color;
 
 public:
-	Harvest(std::string harvestName, float weight, std::string color)
+	Harvest(const std::string& harvestName, float weight, const std::string& color)
 		: _harvestName(harvestName), _weight(weight), _color(color) {}
+
+	virtual ~Harvest() {}
 
 	std::string GetHarvestName()
 	{
@@ -120,6 +137,7 @@ public:
 
 	virtual void PrintHarvest() = 0;
 
+	
 };
 
 class Plant
@@ -130,8 +148,10 @@ private:
 	int _amountHarvest;
 
 public:
-	Plant(std::string plantName, std::string size, int amountHarvest)
+	Plant(const std::string& plantName, const std::string& size, int amountHarvest)
 		: _plantName(plantName), _size(size), _amountHarvest(amountHarvest) {}
+
+	virtual ~Plant() {}
 
 	std::string GetPlantName()
 	{
@@ -143,13 +163,16 @@ public:
 		return _size;
 	}
 
-	int GetAmountOfHarvest()
+	int& GetAmountOfHarvest()
 	{
 		return _amountHarvest;
 	}
 
+
+
 	virtual void PrintPlant() = 0;
 
+	virtual Harvest* GetHarvest() = 0;
 
 };
 
@@ -166,10 +189,11 @@ public:
 	void PrintHarvest() override
 	{
 		std::cout << "Вид плода: " << GetHarvestName() << "\nВес плода: " << GetWeight() << "\nЦвет плода: " << GetColor() << std::endl;
-		std::cout << "Сорт яблок: " << _sortOfApple << std::endl;
+		std::cout << "Сорт: " << _sortOfApple << std::endl;
 	}
 
 };
+
 
 class AppleTree : public Plant
 {
@@ -186,12 +210,73 @@ public:
 		std::cout << "Название растения: " << _name << std::endl;
 	}
 
+	Harvest* GetHarvest() override
+	{
+		int amount = GetAmountOfHarvest()--;
+		if(amount > 0)
+			return new Apple("яблоко", 0.12, "красный", "антоновка");
+		return nullptr;
+	}
+
+	
+
+};
+
+class PeachTree : public Plant
+{
+private:
+	std::string _name;
+
+public:
+	PeachTree(std::string plantName, std::string size, int amountHarvest, std::string name)
+		: Plant(plantName, size, amountHarvest), _name(name) {}
+
+	void PrintPlant()
+	{
+		std::cout << "Вид растения: " << GetPlantName() << "\nРазмер: " << GetSizeOfPlant() << "\nКоличество плодов: " << GetAmountOfHarvest() << std::endl;
+		std::cout << "Название растения: " << _name << std::endl;
+	}
+
+	Harvest* GetHarvest() override
+	{
+		int amount = GetAmountOfHarvest()--;
+		if (amount > 0)
+			return new Apple("персик", 0.15, "красный", "принц");
+		return nullptr;
+	}
+
+};
+
+class SpruceTree : public Plant
+{
+private:
+	std::string _name;
+
+public:
+	SpruceTree(std::string plantName, std::string size, int amountHarvest, std::string name)
+		: Plant(plantName, size, amountHarvest), _name(name) {}
+
+	void PrintPlant()
+	{
+		std::cout << "Вид растения: " << GetPlantName() << "\nРазмер: " << GetSizeOfPlant() << "\nКоличество плодов: " << GetAmountOfHarvest() << std::endl;
+		std::cout << "Название растения: " << _name << std::endl;
+	}
+
+	Harvest* GetHarvest() override
+	{
+		int amount = GetAmountOfHarvest()--;
+		if (amount > 0)
+			return new Apple("шишка", 0.05, "коричневый", "еловая");
+		return nullptr;
+	}
+
 };
 
 
 int main()
 {
 	system("chcp 1251");
+	system("cls");
 	/*Base obj;
 	obj.publicMember;
 	Derived objDer;
@@ -199,7 +284,35 @@ int main()
 	Derived2 objDer2;
 	Derived3 objDer3;*/
 
+	/*Base* obj;
+	obj = new Derived;*/
 
+	AppleTree appleTree("дерево", "средний", 2, "яблоко");
+	PeachTree peachTree("дерево", "средний", 3, "персик");
+	SpruceTree firTree("дерево", "большой", 4, "шишка");
+
+	Harvest* h = appleTree.GetHarvest();
+	while (h != nullptr)
+	{
+		h->PrintHarvest(); std::cout << std::endl;
+		delete h;
+		h = appleTree.GetHarvest();
+	}
+	Harvest* h1 = peachTree.GetHarvest();
+	while (h1 != nullptr)
+	{
+		h1->PrintHarvest(); std::cout << std::endl;
+		delete h1;
+		h1 = peachTree.GetHarvest();
+	}
+	Harvest* h2 = firTree.GetHarvest();
+	while (h2 != nullptr)
+	{
+		h2->PrintHarvest(); std::cout << std::endl;
+		delete h2;
+		h2 = firTree.GetHarvest();
+	}
+	
 	return 0;
 }
 
